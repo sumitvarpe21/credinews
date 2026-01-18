45ab8e17da8f4ef5b8395ecdc5434b075f6be145
sumitvarpe21 <EMAIL_USED_ON_GITHUB>
2026-01-18 22:25:26 +0530

    Fix cross-app imports

--- backend/ratings/models.py
+++ backend/ratings/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from news.models import Article
+from backend.news.models import Article
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
