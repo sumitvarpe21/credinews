1bdc8161f15766a2a672f70e78767f0afdbc3665
sumitvarpe21 <EMAIL_USED_ON_GITHUB>
2026-01-18 22:54:43 +0530

    Fix remaining news import paths

--- backend/news/admin_views.py
+++ backend/news/admin_views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
-from news.models import Article
+from backend.news.models import Article
 from ratings.models import Rating
 
 @api_view(['GET'])
